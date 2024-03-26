de2d6e34a5b6d1df2fca80493eb3378eaf693ec5
San-QP <s.rebollar.g@gmail.com>
2024-03-26 22:08:35 +0100

    added and styled mi-huerto page

--- frontend/src/pages/MiHuerto.jsx
+++ frontend/src/pages/MiHuerto.jsx
@@ -1,8 +1,39 @@
+import { Link } from "react-router-dom";
+import { Menu } from "../components/Menu";
+import "../styles/MiHuerto.css";
 
 export const MiHuerto = () => {
   return (
-    <div>
-        MiHuerto
-    </div>
-  )
-}
+    <>
+      <header>
+        <Link to="/panel">VOLVER A INICIO</Link>
+      </header>
+      <main className="panel_MiHuerto">
+        <div className="panelContent">
+          <section className="panelContent_title">
+            <img
+              src="../../public/MiHuertoLogo.svg"
+              alt="Logo Appio"
+              style={{ minWidth: "30rem" }}
+            />
+            <p>
+              Mantén tu huerto organizado guardando un registro de tu biblioteca
+              de semillas y tus cultivos.
+            </p>
+          </section>
+          <section className="panel_MiHuerto_buttons">
+            <div>
+              <img src="../../public/mis-cultivos.jpg" alt="" />
+              <a>MIS SEMILLAS</a>
+            </div>
+            <div>
+                <img src="../../public/mis-cultivos.jpg" alt="" />
+              <a>MIS CULTIVOS</a>
+            </div>
+          </section>
+          <Menu />
+        </div>
+      </main>
+    </>
+  );
+};
