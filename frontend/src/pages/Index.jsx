1bfc09d45b1d8423d05d11e6017e5c6957b0282d
San-QP <s.rebollar.g@gmail.com>
2024-03-26 16:14:50 +0100

    Index page added

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,11 +1,21 @@
-import { MainNav } from "../components/MainNav"
+import { MainNav } from "../components/MainNav";
+import "../styles/Index.css";
 
 export const Index = () => {
   return (
     <>
-        <header>
-            <MainNav/>
-        </header>
+      <header>
+        <MainNav />
+      </header>
+      <main className="index">
+        <div className="indexContent">
+            <img
+            src="../../public/AppioLogo.svg"
+            alt="Appio Logo Soberanía Alimentaria"
+            />
+            <a href="#">INICIAR SESIÓN</a>
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
