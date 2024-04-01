ced8868b4f06df86fd2898a10fc31cc9c6444bd2
San-QP <s.rebollar.g@gmail.com>
2024-04-01 21:11:56 +0200

    Frontend: Post method not working

--- frontend/src/components/ModalNewSeed.jsx
+++ frontend/src/components/ModalNewSeed.jsx
@@ -1,12 +1,42 @@
 import "../styles/ModalNewSeed.css";
 import { IconClose } from "../assets/svg/IconClose";
 import { useState } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 
 export const ModalNewSeed = ({ closeModal }) => {
   const [selectedOption, setSelectedOption] = useState("");
+  const [postSeed, setPostSeed] = useState({
+    name: "",
+    origin: "",
+    pickUpDate: "",
+    generation: "",
+    description: "",
+  });
 
-  const handleOptionChange  = (event) => {
+  const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
+  };
+
+  const handleInput = (event) => {
+    setPostSeed({ ...postSeed, [event.target.name]: event.target.value });
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = {
+      name: postSeed.name,
+      origin: postSeed.origin,
+      pickUpDate: postSeed.pickUpDate,
+      generation: postSeed.generation,
+      description: postSeed.description,
+    };
+
+    axios
+      .post("http://localhost:8080/api/seeds", data)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -14,25 +44,31 @@ export const ModalNewSeed = ({ closeModal }) => {
       <div className="newSeed_Modal">
         <header className="newSeed_Modal_Header">
           <h1>Nueva especie</h1>
-          <button
-            className="buttonClose"
-            onClick={ closeModal }>
-           <IconClose/>
+          <button className="buttonClose" onClick={closeModal}>
+            <IconClose />
           </button>
         </header>
-        <body className="newSeed_Modal_Content">
-          <form action="" className="newSeed_Modal_Content_Form">
+        <main className="newSeed_Modal_Content">
+          <form onSubmit={handleSubmit} className="newSeed_Modal_Content_Form">
             <section className="newSeed_Modal_Content_Form_Inputs">
               <input
                 type="text"
                 placeholder="Nombre de la especie"
                 className="seedName"
+                onChange={handleInput}
               />
               <fieldset className="newSeed_Modal_Content_Form_InputsProps">
                 <div className="inputProps_From">
                   <label htmlFor="">¿De dónde vienen tus semillas?</label>
 
-                  <select className="seedFrom" onChange={handleOptionChange} value={selectedOption}>
+                  <select
+                    className="seedFrom"
+                    onChange={(event) => {
+                      handleOptionChange(event);
+                      handleInput(event);
+                    }}
+                    value={selectedOption}
+                  >
                     <option value="">-- Selecciona el origen --</option>
                     <option value="Recogida">Recogida</option>
                     <option value="Intercambio">Intercambio</option>
@@ -40,23 +76,33 @@ export const ModalNewSeed = ({ closeModal }) => {
                   </select>
                 </div>
                 {selectedOption === "Recogida" && (
-                <>
-                <div className="pickUp_Options_Active">
-                  <label htmlFor="">Fecha de recogida:</label>
-                  <input type="date" className="seedDate" />
-                </div>
-                <div className="pickUp_Options_Active">
-                  <label htmlFor="">Generación:</label>
-                  <input type="number" className="seedGeneration" min="0"/>
-                </div>
-                </>
+                  <>
+                    <div className="pickUp_Options_Active">
+                      <label htmlFor="">Fecha de recogida:</label>
+                      <input
+                        type="date"
+                        className="seedDate"
+                        onChange={handleInput}
+                      />
+                    </div>
+                    <div className="pickUp_Options_Active">
+                      <label htmlFor="">Generación:</label>
+                      <input
+                        type="number"
+                        className="seedGeneration"
+                        min="0"
+                        onChange={handleInput}
+                      />
+                    </div>
+                  </>
                 )}
               </fieldset>
               <fieldset className="newSeed_Modal_Content_Form_InputsDetails">
                 <label htmlFor="">Más detalles:</label>
-                <textarea placeholder="Cantidad de semillas, características de la especie, resistencias a hongos y plagas, consejos de siembra...">
-                </textarea>
-
+                <textarea
+                  placeholder="Cantidad de semillas, características de la especie, resistencias a hongos y plagas, consejos de siembra..."
+                  onChange={handleInput}
+                ></textarea>
               </fieldset>
             </section>
             <div className="newSeed_Modal_Content_Form_Button">
@@ -65,8 +111,12 @@ export const ModalNewSeed = ({ closeModal }) => {
               </button>
             </div>
           </form>
-        </body>
+        </main>
       </div>
     </section>
   );
+};
+
+ModalNewSeed.propTypes = {
+  closeModal: PropTypes.func.isRequired,
 };
