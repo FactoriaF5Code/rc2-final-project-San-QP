915ececc94f8b99f1edbf2cd978260bd70a500b0
San-QP <s.rebollar.g@gmail.com>
2024-04-18 09:51:31 +0200

    Fixing styles at advertPage

--- frontend/src/pages/AdvertPage/AdvertPage.jsx
+++ frontend/src/pages/AdvertPage/AdvertPage.jsx
@@ -2,43 +2,42 @@ import "../MiHuerto/MiHuerto.css";
 import "../Comunidad/Comunidad.css";
 import "./AdvertPage.css";
 import { BackHomeLink } from "../../components/BackHomeLink";
-import {IconClose} from "../../assets/svg/IconClose"
+import { IconClose } from "../../assets/svg/IconClose";
 import { Menu } from "../../components/Menu/Menu";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { AdvertsContext } from "../../middleware/context/AdvertsContext";
 
-
 export const AdvertPage = () => {
-    const { advertId } = useParams();
-    const navigate = useNavigate();
-    const { adverts } = useContext(AdvertsContext);
-    const [seedName, setSeedName] = useState("");
-    const [seedDescription, setSeedDescription] = useState("");
-    const [seedOrigin, setSeedOrigin] = useState("");
-    const [seedUser, setSeedUser] = useState("");
-    const [seedImg, setSeedImg] = useState("");
+  const { advertId } = useParams();
+  const navigate = useNavigate();
+  const { adverts } = useContext(AdvertsContext);
+  const [seedName, setSeedName] = useState("");
+  const [seedDescription, setSeedDescription] = useState("");
+  const [seedOrigin, setSeedOrigin] = useState("");
+  const [seedUser, setSeedUser] = useState("");
+  const [seedImg, setSeedImg] = useState("");
 
-    useEffect(() => {
-        const advert = adverts.find((advert) => advert.id === Number(advertId));
-        if (advert) {
-            setSeedName(advert.seed.name);
-            setSeedOrigin(advert.seed.origin);
-            setSeedDescription(advert.seed.description);
-            setSeedUser(advert.user.name);
-            setSeedImg(advert.url_img);
-        }
-    }, [advertId, adverts]);
-  
-    const contactUser = () => {
-        navigate("/wait-for-it");
+  useEffect(() => {
+    const advert = adverts.find((advert) => advert.id === Number(advertId));
+    if (advert) {
+      setSeedName(advert.seed.name);
+      setSeedOrigin(advert.seed.origin);
+      setSeedDescription(advert.seed.description);
+      setSeedUser(advert.user.name);
+      setSeedImg(advert.url_img);
     }
+  }, [advertId, adverts]);
 
-    const closeAdvert = () => {
-      navigate("/comunidad");
-  }
+  const contactUser = () => {
+    navigate("/wait-for-it");
+  };
 
-    return (
+  const closeAdvert = () => {
+    navigate("/comunidad");
+  };
+
+  return (
     <>
       <header className="backLinkAdvert">
         <BackHomeLink />
@@ -55,23 +54,29 @@ export const AdvertPage = () => {
         </section>
         <section className="singleAdvert">
           <div className="singleAdvertImg">
-            <button className="iconClose" onClick={closeAdvert}><IconClose /></button>
+            <button className="iconClose" onClick={closeAdvert}>
+              <IconClose />
+            </button>
             <img src={seedImg} alt={seedName} />
           </div>
           <div className="singleAdvertInfo">
             <div>
               <h2>{seedName}</h2>
-              <p className="singleAdvertDetails">Origen de las semillas: {seedOrigin}</p>
+              <p className="singleAdvertDetails">
+                Origen de las semillas: {seedOrigin}
+              </p>
               <p className="singleAdvertDescription">{seedDescription}</p>
             </div>
             <div>
               <p className="singleAdvertDetails">USUARIO: {seedUser}</p>
-              <button className="contactButton" onClick={contactUser}>CONTACTAR</button>
+              <button className="contactButton" onClick={contactUser}>
+                CONTACTAR
+              </button>
             </div>
           </div>
         </section>
       </main>
-      
+
       <Menu />
     </>
   );
